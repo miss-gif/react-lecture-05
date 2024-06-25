3ad9b80b6ca52896dfb52caad5c89455661f5eb0
devgreact <dev.greact@gmail.com>
2024-06-25 13:14:26 +0900

    docs : css 기초

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Home } from "./pages/Home";
-
+import "./App.css";
+import { WrapStyle, DivStyle } from "./css/AppCss";
 const App = () => {
   // js 자리
-
   return (
-    <div className="wrap">
+    <div className="wrap" style={WrapStyle}>
       <Home />
     </div>
   );
---
+++ src/css/AppCss.js
@@ -0,0 +1,2 @@
+export const WrapStyle = { backgroundColor: "red", fontSize: "12px" };
+export const DivStyle = { backgroundColor: "red", fontSize: "12px" };
