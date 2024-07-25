68fa785478b70028e22e7b014d5256ee185a8839
devgreact <dev.greact@gmail.com>
2024-07-25 13:22:06 +0900

    진행중

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Counter from "./pages/Counter";
 import LoginForm from "./pages/LoginForm";
+import Join from "./pages/Join";
 
 const App = () => {
   return (
     <div>
       <h1> APP 컴포넌트</h1>
-      <Counter />
-      <LoginForm />
+      <Join />
     </div>
   );
 };
--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,15 +1,6 @@
-# React Hook Form
-
-- https://react-hook-form.com/
-- `npm install react-hook-form`
-
-## 1. React Hook Form 기본활용
-
-- 리랜더링을 최소화 함.
-- /src/pages/Join.js
-
-```js
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 // 폼의 초기값
 const initState = {
@@ -19,6 +10,25 @@ const initState = {
   phone: "010-0000-0000",
   address1: "080",
 };
+
+// yup schema 셋팅
+const schema = yup.object().shape({
+  userid: yup.string().required("아이디는 필수 입니다."),
+  email: yup
+    .string()
+    .required("이메일은 필수 항목입니다.")
+    .email("유효한 이메일 주소를 입력하세요."),
+  pass: yup
+    .string()
+    .required("비밀번호를 입력해주세요")
+    .min(8, "비밀번호는 최소 8자입니다.")
+    .max(16, "비밀번호는 최대 16자입니다"),
+  phone: yup
+    .string()
+    .required("전화번호를 입력해주세요")
+    .matches(/^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$/, "유효한 전화번로를 입력하세요."),
+  address1: yup.string().required("우편번호를 입력해주세요"),
+});
 
 const Join = () => {
   // form 의 상태를 관리하는 기능
@@ -32,7 +42,7 @@ const Join = () => {
     handleSubmit,
     setValue,
     formState: { errors },
-  } = useForm({ defaultValues: initState });
+  } = useForm({ defaultValues: initState, resolver: yupResolver(schema) });
 
   //   전화번호 자동 변경
   //   전화번호 자동 변경
@@ -63,55 +73,24 @@ const Join = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>아이디</label>
-          <input
-            type="text"
-            {...register("userid", { required: "아이디는 필수 입니다." })}
-          />
+          <input type="text" {...register("userid")} />
           {errors.userid && <span>{errors.userid.message}</span>}
         </div>
         <div>
           <label>이메일</label>
-          <input
-            type="email"
-            {...register("email", {
-              required: "이메일은 필수 항목입니다.",
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: "유효한 이메일 주소를 입력하세요.",
-              },
-            })}
-          />
+          <input type="email" {...register("email")} />
           {errors.email && <span>{errors.email.message}</span>}
         </div>
         <div>
           <label>비밀번호</label>
-          <input
-            type="password"
-            {...register("pass", {
-              required: "비밀번호는 필수 항목입니다.",
-              minLength: {
-                value: 8,
-                message: "비밀번호는 최소 8자 입니다.",
-              },
-              maxLength: {
-                value: 16,
-                message: "비밀번호는 최대 16자 입니다.",
-              },
-            })}
-          />
+          <input type="password" {...register("pass")} />
           {errors.pass && <span>{errors.pass.message}</span>}
         </div>
         <div>
           <label>전화번호</label>
           <input
             type="text"
-            {...register("phone", {
-              required: "전화번호는 필수 항목입니다.",
-              pattern: {
-                value: /^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$/,
-                message: "유효한 전화번호를 입력하세요.",
-              },
-            })}
+            {...register("phone")}
             onChange={e => {
               handleChangePhone(e);
             }}
@@ -120,12 +99,7 @@ const Join = () => {
         </div>
         <div>
           <label>주소</label>
-          <input
-            type="text"
-            {...register("address1", {
-              required: "우편번호는 필수 항목입니다.",
-            })}
-          />
+          <input type="text" {...register("address1")} />
           {errors.address1 && <span>{errors.address1.message}</span>}
         </div>
         <button type="submit">회원가입</button>
@@ -136,24 +110,3 @@ const Join = () => {
 };
 
 export default Join;
-```
-
-## 2. Yup 활용
-
-- 입력내용의 유효성 검사 라이브러리
-
-## 2.1. 설치
-
-- `npm i yup`
-- https://github.com/jquense/yup
-
-## 2.2. Hook Form 에 적용
-
-- React Hook Form 에서 활용시 추가 설치
-  : https://www.npmjs.com/package/@hookform/resolvers
-  : `npm i @hookform/resolvers`
-- https://www.react-hook-form.com/advanced-usage/#CustomHookwithResolver
-
-## 2.3. 적용 단계
-
-- yup schema 셋팅
