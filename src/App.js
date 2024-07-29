5c66ffaff9b6910054e2ab36826432b6eac37761
devgreact <dev.greact@gmail.com>
2024-07-29 12:58:18 +0900

    React Quill 활용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
-import Counter from "./pages/Counter";
-import LoginForm from "./pages/LoginForm";
-import Join from "./pages/Join";
+import Write from "./pages/Write";
 
 const App = () => {
   return (
     <div>
       <h1> APP 컴포넌트</h1>
-      <Join />
+      <Write />
     </div>
   );
 };
