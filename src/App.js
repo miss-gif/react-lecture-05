f80ed1891ff408802da74d5fa518a8d196e75dd8
devgreact <dev.greact@gmail.com>
2024-07-30 11:43:20 +0900

    React Query 기본활용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Write from "./pages/Write";
+import { useState } from "react";
+import PostAdd from "./pages/PostAdd";
+import PostList from "./pages/PostList";
+import PostUpdate from "./pages/PostUpdate";
 
 const App = () => {
+  const [item, setItem] = useState(null);
   return (
     <div>
       <h1> APP 컴포넌트</h1>
-      <Write />
+      <PostList setItem={setItem} />
+      <PostUpdate item={item} />
+      <PostAdd />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import { RecoilRoot } from "recoil";
-
-// ts 에서는 데이터 종류를 구별한다.
-// as 는 강제로 타입지정
-// const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
+import "./index.css";
 // js 버전
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// 웹 서비스 연결 및 결과에 대한 상태관리
+const queryClient = new QueryClient();
+
 root.render(
-  <RecoilRoot>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </RecoilRoot>,
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>,
 );
