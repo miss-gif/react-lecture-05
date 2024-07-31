9dc9775ea20693ba339df7b22c54ffc6973ed811
devgreact <dev.greact@gmail.com>
2024-07-31 11:02:12 +0900

    카카오 로그인 API 연동

--- src/index.js
+++ src/index.js
@@ -6,12 +6,4 @@ import "./index.css";
 // js 버전
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// 웹 서비스 연결 및 결과에 대한 상태관리
-const queryClient = new QueryClient();
-
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={true} />
-  </QueryClientProvider>,
-);
+root.render(<App />);
