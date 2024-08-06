592858b3e9baf41ed263a4eceab6609b4aeb71d8
devgreact <dev.greact@gmail.com>
2024-08-06 11:06:31 +0900

    FB 로그인 및 회원가입 UI

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,2 +1,8 @@
-const useAuth = () => {};
+import { useState } from "react";
+
+const useAuth = () => {
+  const [user, setUser] = useState(false);
+
+  return { user };
+};
 export default useAuth;
